95ab1647f28f678571b45d2860fea43cf05fe1ed
Satyam Katiyar <anshkatiyar019@gmail.com>
2025-08-20 20:09:35 +0530

    update

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -159,7 +159,7 @@ const Login = () => {
                             <p className='text-gray-600'>
                                 Don't have an account?{' '}
                                 <Link to="/signup" className='text-blue-600 hover:text-blue-700 font-medium transition-colors duration-300'>
-                                    Sign up here
+                                    Sign up
                                 </Link>
                             </p>
                         </div>
